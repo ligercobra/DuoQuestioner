c12fa5b447aba4b30d49d9ddf8ab61b52b7d90c5
nato6933 <nato6933@room6933.com>
2012-03-16 23:47:27 +0900

    add new table

--- Model/UserLogic.py
+++ Model/UserLogic.py
@@ -13,28 +13,42 @@ class BaseModel(peewee.Model):
         database = MYDB
 
 class AnsTB(BaseModel):
-    ansid = peewee.PrimaryKeyField()
-    usid = peewee.IntegerField()
-    secid = peewee.IntergerField()
-    anstime = peewee.DateTimeField()
-    ans = peewee.IntergerField()
+    ansid = peewee.PrimaryKeyField() #primary key index
+    usid = peewee.IntegerField() #user id
+    qid = peewee.IntegerField() #question id
+    secid = peewee.IntergerField() #section id
+    anstime = peewee.DateTimeField() #time of answer
+    ans = peewee.IntergerField() #user's answer(1 or 0)
 
 class LastTB(BaseModel):
-    lansid = peewee.PrimaryKeyField()
-    usid = peewee.IntegerField()
-    secid = peewee.IngerField()
-    anstime = peewee.DateTimeField()
-    ans = peewee.IntegerField()
+    lid = peewee.PrimaryKeyField() #primary key index
+    usid = peewee.IntegerField() #user id
+    qid = peewee.IntegerField() #question id
+    secid = peewee.IngerField() #section id
+    anstime = peewee.DateTimeField() #time of answer
+    ans = peewee.IntegerField() #user's answer(1 or 0)
 
 class QuesTB(BaseModel):
-    qid = peewee.PrimaryKeyField()
-    secid = peewee.IntegerField()
-    en = peewee.TextField()
-    jp = peewee.TextField()
+    qid = peewee.PrimaryKeyField() #quesiton id,primary key index
+    en = peewee.TextField() #duo english text
+    jp = peewee.TextField() #duo japanese text
+    secid = peewee.IntegerField() #section id
+
+class SecNum(BaseModel):
+    secid = peewee.PrimaryKeyField() #section id
+    qnum = peewee.PrimaryKeyField() #section's qustion number
 
 class UserLogic(object):
     def __init__(self):
         AnsTB.create_table(True)
         LastTB.create_table(True)
         QuesTB.create_table(True)
+        SecNum.create_table(True)
+
+class UserSelect(UserLogic):
+    
+    def last_by_section(self,usid,secid,anstime=None,ans=None):
+        for i in secid:
+
+        sq = LastTB.select().where( 
 
